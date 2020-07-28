ce659f8ef0cdb15aa679dc968eae09fde57324a2
Fialkaksu <fialkaksu1984@gmail.com>
2020-07-28 18:07:33 +0300

    Updates

---
+++ precache-manifest.d96b630275cd09bb2dbe2b5f7ddf7d3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2595af7bb91697f19a7dd68b08829f",
+    "url": "/Yalantis/index.html"
+  },
+  {
+    "revision": "79a2615cbdcd8f23c3b3",
+    "url": "/Yalantis/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "83574b21699af702dcfe",
+    "url": "/Yalantis/static/css/main.4809c81d.chunk.css"
+  },
+  {
+    "revision": "79a2615cbdcd8f23c3b3",
+    "url": "/Yalantis/static/js/2.c0b4803b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Yalantis/static/js/2.c0b4803b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83574b21699af702dcfe",
+    "url": "/Yalantis/static/js/main.0cb86348.chunk.js"
+  },
+  {
+    "revision": "aa8c344c91dfc608d3c1",
+    "url": "/Yalantis/static/js/runtime-main.a2b08f02.js"
+  }
+]);
