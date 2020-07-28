61cdfda527df4d2bbdf8e92944b6e1dd4cf356dd
Oksana Dmytriieva <46996947+Fialkaksu@users.noreply.github.com>
2020-07-28 18:33:52 +0300

    del comments

--- src/utils/month.js
+++ src/utils/month.js
@@ -1,78 +1,62 @@
-// export const MONTH = ['January', 'February', 'March', 'April', 'May', 'June',
-//   'July', 'August', 'September', 'October', 'November', 'December'
-// ];
-
 export const MONTH = [{
     name: 'January',
     num: 0,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'February',
     num: 1,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'March',
     num: 2,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'April',
     num: 3,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'May',
     num: 4,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'June',
     num: 5,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'July',
     num: 6,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'August',
     num: 7,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'September',
     num: 8,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'October',
     num: 9,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'November',
     num: 10,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   },
   {
     name: 'December',
     num: 11,
-    count: 0,
-    // colorIndicator: undefined
+    count: 0
   }
 ]
 
